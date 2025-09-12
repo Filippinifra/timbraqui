bd6903d2004f7cb1027dbd211607f8151648f741
Filippinifra <filippinifra123@gmail.com>
2025-09-12 17:03:23 +0200

    add auth image for background

--- src/views/auth.tsx
+++ src/views/auth.tsx
@@ -9,51 +9,62 @@ export const AuthView: FC<{ type: "login" | "register" }> = ({ type }) => {
   return (
     <div
       style={{
-        margin: "auto",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
-        gap: 8,
+        margin: 0,
+        backgroundColor: colors.greyExtraLight,
+        backgroundImage:
+          "linear-gradient(rgba(255, 255, 255, .90), rgba(255, 255, 255, .90)), url(/office.jpg)",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
       }}
     >
-      <Typography variant="p-xs-sb" color={colors.grey}>
-        Continuando accetti automaticamente la nostra{" "}
-        <Typography
-          variant="p-xs-sb"
-          color={colors.grey}
-          component="a"
-          style={{ textDecoration: "underline" }}
-          href={Routes.cookies}
-        >
-          Cookie policy
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          flexDirection: "column",
+          gap: 8,
+        }}
+      >
+        <Typography variant="p-xs-sb" color={colors.grey}>
+          Continuando accetti automaticamente la nostra{" "}
+          <Typography
+            variant="p-xs-sb"
+            color={colors.grey}
+            component="a"
+            style={{ textDecoration: "underline" }}
+            href={Routes.cookies}
+          >
+            Cookie policy
+          </Typography>
+          {" e i nostri "}
+          <Typography
+            variant="p-xs-sb"
+            color={colors.grey}
+            component="a"
+            style={{ textDecoration: "underline" }}
+            href={Routes.termsConditionsGDPR}
+          >
+            Termini e condizioni
+          </Typography>
         </Typography>
-        {" e i nostri "}
-        <Typography
-          variant="p-xs-sb"
-          color={colors.grey}
-          component="a"
-          style={{ textDecoration: "underline" }}
-          href={Routes.termsConditionsGDPR}
-        >
-          Termini e condizioni
-        </Typography>
-      </Typography>
-      {type === "login" ? (
-        <SignIn
-          signUpUrl={Routes.register}
-          fallbackRedirectUrl={Routes.dashboard}
-          fallback={<Placeholder height={526} width={"100%"} />}
-        />
-      ) : (
-        <SignUp
-          signInUrl={Routes.login}
-          fallbackRedirectUrl={Routes.dashboard}
-          signInFallbackRedirectUrl={Routes.dashboard}
-          fallback={<Placeholder height={606} width={"100%"} />}
-        />
-      )}
+        {type === "login" ? (
+          <SignIn
+            signUpUrl={Routes.register}
+            fallbackRedirectUrl={Routes.dashboard}
+            fallback={<Placeholder height={526} width={"100%"} />}
+          />
+        ) : (
+          <SignUp
+            signInUrl={Routes.login}
+            fallbackRedirectUrl={Routes.dashboard}
+            signInFallbackRedirectUrl={Routes.dashboard}
+            fallback={<Placeholder height={606} width={"100%"} />}
+          />
+        )}
+      </div>
     </div>
   );
 };
